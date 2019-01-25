6c71cb1dc339bd8cd30c7fe099f5c6bff69076ec
vrscience <vrscience>
2019-01-25 13:38:35 +0100

    Implemented choices for data exploration Implemented TF session to run Implemented validation test Fixed some issue

--- TF_trafficSigns_using_my_Loader.py
+++ TF_trafficSigns_using_my_Loader.py
@@ -53,7 +53,7 @@ y = tf.placeholder(dtype=tf.int32, shape=[None])
 x_flat = tf.contrib.layers.flatten(x)
 
 #Create fully connected layers
-logits= tf.contrib.layers.fully_connected(logits, 62, tf.nn.relu)
+logits = tf.contrib.layers.fully_connected(x_flat, 62, tf.nn.relu)
 
 #Define Loss Function
 loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,
